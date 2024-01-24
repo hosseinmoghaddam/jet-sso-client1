ae00cedd4aed034a29f4e83056f425c7e2c1aa1f
hossein-moghadam <hosseinghmoghadam@gmail.com>
2024-01-24 08:15:51 +0330

    oss test

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,9 +14,28 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+//Route::get('/callback', function (Request $request){
+//    return
+//});
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/auth/callback', function (Request $request) {
+    $state = $request->session()->pull('state');
+
+    throw_unless(
+        strlen($state) > 0 && $state === $request->state,
+        InvalidArgumentException::class,
+        'Invalid state value.'
+    );
+
+    $response = Http::asForm()->post('http://localhost:8080/oauth/token', [
+        'grant_type' => 'authorization_code',
+        'client_id' => '9b29833f-1ef0-43b2-8ecd-ba5d4b204e00',
+        'client_secret' => '9uITfUONHLTh1bidPBusjKlrJcgufp1brR5N9znC',
+        'redirect_uri' => 'http://127.0.0.1:8000/auth/callback',
+        'code' => $request->code,
+    ]);
+
+    return $response->json();
 });
 
 Route::middleware([
@@ -25,4 +46,19 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+});
+
+Route::get('/redirect', function (Request $request) {
+    $request->session()->put('state', $state = Str::random(40));
+
+    $query = http_build_query([
+        'client_id' => '9b29833f-1ef0-43b2-8ecd-ba5d4b204e00',
+        'redirect_uri' => 'http://127.0.0.1:8000/auth/callback',
+        'response_type' => 'code',
+        'scope' => '',
+        'state' => $state,
+         'prompt' => 'login', // "none", "consent", or "login"
+    ]);
+
+    return redirect('http://localhost:8080/oauth/authorize?'.$query);
 });
